2c5a52d2aab2c0680f1ee117c6096e0eca65b95c
Arjun Dilip Kumar <arjundilipkumar93@gmail.com>
2015-01-17 04:20:33 -0500

    Hiding Keyboard after input in Home

---
+++ TripMe Flights/TripMe Flights/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  TripMe Flights
+//
+//  Created by Arjun Dilip Kumar on 1/17/15.
+//  Copyright (c) 2015 Princes of Norfwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ TripMe Flights/TripMe Flights/HomeViewController.m
@@ -0,0 +1,46 @@
+//
+//  HomeViewController.m
+//  TripMe Flights
+//
+//  Created by Arjun Dilip Kumar on 1/17/15.
+//  Copyright (c) 2015 Princes of Norfwood. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)dismissKeyboard {
+    [self.view endEditing:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
