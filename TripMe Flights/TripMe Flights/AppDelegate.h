0f2ae7cee6091a79230498373eb472661498813e
Arjun Dilip Kumar <arjundilipkumar93@gmail.com>
2015-01-17 01:00:38 -0500

    First Commit

---
+++ TripMe Flights/TripMe Flights/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TripMe Flights
+//
+//  Created by Arjun Dilip Kumar on 1/17/15.
+//  Copyright (c) 2015 Princes of Norfwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TripMe Flights/TripMe Flights/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TripMe Flights
+//
+//  Created by Arjun Dilip Kumar on 1/17/15.
+//  Copyright (c) 2015 Princes of Norfwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
